001a4f78d18dcac4dbc9871f6fa91974a4b0029a
Jörg Zimmermann <Jaerschi@gmx.de>
2015-04-19 00:05:22 +0200

    Menu check state store and reload with SharedPreferences

--- IdentifyPrankCaller/app/src/main/java/hs_kl/de/identifyprankcaller/PrankCallerIdentification.java
+++ IdentifyPrankCaller/app/src/main/java/hs_kl/de/identifyprankcaller/PrankCallerIdentification.java
@@ -1,6 +1,7 @@
 package hs_kl.de.identifyprankcaller;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
@@ -16,35 +17,55 @@ import threadimplementations.QuerierAsyncTask;
 
 
 public class PrankCallerIdentification extends Activity implements AsyncResponse {
+    public static final String PREFS_NAME = "PCIPrefsFile";
+    private static final int MENU_SIZE = 5;
+    public static final int MENU_TASK_METHOD_GUI = 0;
+    public static final int MENU_TASK_METHOD_THREAD = 1;
+    public static final int MENU_TASK_METHOD_ASYNC = 2;
+    public static final int MENU_SEARCH_ENGINE_GOOGLE = 3;
+    public static final int MENU_SEARCH_ENGINE_OPEN_CNAME = 4;
+
+    private boolean menu_values[] = new boolean[MENU_SIZE];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prank_caller_identification);
+        loadMenuValues();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_prank_caller_identification, menu);
+        restoreVisualMenuCheckState(menu);
         return true;
     }
 
-    @Override
+     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int iid = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_tasks) {
-            return true;
+         //Set checked state to the selected menu item
+        switch (iid) {
+            case R.id.menu_task_method_gui:
+                return setVisualMenuCheckState(MENU_TASK_METHOD_GUI, "menu_task_methods", item);
+            case R.id.menu_task_method_thread:
+                return setVisualMenuCheckState( MENU_TASK_METHOD_THREAD, "menu_task_methods", item );
+            case R.id.menu_task_method_async:
+                return setVisualMenuCheckState( MENU_TASK_METHOD_ASYNC, "menu_task_methods", item );
+            case R.id.menu_search_engine_google:
+                return setVisualMenuCheckState( MENU_SEARCH_ENGINE_GOOGLE, "menu_search_engines", item );
+            case R.id.menu_search_engine_open_cname:
+                return setVisualMenuCheckState( MENU_SEARCH_ENGINE_OPEN_CNAME, "menu_search_engines", item );
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 
     public void startSearchAsyncTask(View view)
     {
@@ -88,4 +109,62 @@ public class PrankCallerIdentification extends Activity implements AsyncResponse
         TextView resultText = (TextView) findViewById(R.id.resultText);
         resultText.setText(result.getDescription());
     }
+
+
+
+    public void loadMenuValues() {
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        menu_values[MENU_TASK_METHOD_GUI] = settings.getBoolean("menu_task_method_gui", false);
+        menu_values[MENU_TASK_METHOD_THREAD] = settings.getBoolean("menu_task_method_thread", false);
+        menu_values[MENU_TASK_METHOD_ASYNC] = settings.getBoolean("menu_task_method_async", false);
+        menu_values[MENU_SEARCH_ENGINE_GOOGLE] = settings.getBoolean("menu_search_engine_google", false);
+        menu_values[MENU_SEARCH_ENGINE_OPEN_CNAME] = settings.getBoolean("menu_search_engine_open_cname", false);
+    }
+
+
+    public void saveMenuValues() {
+        //persist in shared preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("menu_task_method_gui", menu_values[MENU_TASK_METHOD_GUI]);
+        editor.putBoolean("menu_task_method_thread", menu_values[MENU_TASK_METHOD_THREAD]);
+        editor.putBoolean("menu_task_method_async", menu_values[MENU_TASK_METHOD_ASYNC]);
+        editor.putBoolean("menu_search_engine_google", menu_values[MENU_SEARCH_ENGINE_GOOGLE]);
+        editor.putBoolean("menu_search_engine_open_cname", menu_values[MENU_SEARCH_ENGINE_OPEN_CNAME]);
+        editor.commit();
+    }
+
+    //handles all jobs to set a menu item check state
+    public boolean setVisualMenuCheckState( int menuId, String menuGroup, MenuItem item ) {
+        //set checked state visual
+        item.setChecked(!item.isChecked());
+        //unset all item values in group and set clicked one
+        //TODO: Very dirty coded!! Do it a better way!
+        if (menuGroup.equals("menu_task_methods")){
+            menu_values[MENU_TASK_METHOD_GUI] = false;
+            menu_values[MENU_TASK_METHOD_THREAD] = false;
+            menu_values[MENU_TASK_METHOD_ASYNC] = false;
+        }
+        if (menuGroup.equals("menu_search_engines")){
+            menu_values[MENU_SEARCH_ENGINE_GOOGLE] = false;
+            menu_values[MENU_SEARCH_ENGINE_OPEN_CNAME] = false;
+        }
+        menu_values[ menuId ] = true;
+        //persist menu values
+        saveMenuValues();
+
+        return true;
+    }
+
+
+    public void restoreVisualMenuCheckState(Menu menu) {
+        //restore check state values
+        menu.findItem(R.id.menu_task_method_gui).setChecked(menu_values[MENU_TASK_METHOD_GUI]);
+        menu.findItem(R.id.menu_task_method_thread).setChecked(menu_values[MENU_TASK_METHOD_THREAD]);
+        menu.findItem(R.id.menu_task_method_async).setChecked(menu_values[MENU_TASK_METHOD_ASYNC]);
+        menu.findItem(R.id.menu_search_engine_google).setChecked(menu_values[MENU_SEARCH_ENGINE_GOOGLE]);
+        menu.findItem(R.id.menu_search_engine_open_cname).setChecked(menu_values[MENU_SEARCH_ENGINE_OPEN_CNAME]);
+    }
+
 }
